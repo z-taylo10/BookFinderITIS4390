39342dccca82d4c61edafc68f2dd185dcdc0c68c
Sharmeen <shatimbh@charlotte.edu>
2024-10-29 21:00:50 -0400

    Added Landing Screen Prototype

---
+++ bookstore-ui/src/App.js
@@ -0,0 +1,48 @@
+// src/App.js
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import './styles.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { CartProvider } from './context/CartContext';
+import HomePage from './pages/HomePage';
+import SearchResultsPage from './pages/SearchResultsPage';
+import GenrePage from './pages/GenrePage';
+import Cart from './components/Cart';
+import Header from './components/Header';
+import Genres from './components/Genres';
+import AccountsPage from './pages/AccountsPage';
+import Footer from './components/Footer';
+import BookDetailPage from './pages/BookDetailPage';
+
+function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [selectedGenre, setSelectedGenre] = useState('');
+
+  const handleGenreClick = (genre) => {
+    setSelectedGenre(genre);
+  };
+
+  return (
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <Header />
+          <div className="content">
+            <Routes>
+              <Route path="/" element={<HomePage onSearch={setSearchQuery} />} />
+              <Route path="/search" element={<SearchResultsPage query={searchQuery} />} />
+              <Route path="/genres" element={<Genres onGenreClick={handleGenreClick} />} />
+              <Route path="/genre" element={<GenrePage genre={selectedGenre} />} />
+              <Route path="/books/:bookId" element={<BookDetailPage />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/accounts" element={<AccountsPage />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
+  );
+}
+
+export default App;
