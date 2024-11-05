77a677d9193f866a34c61c37899eae424c10a652
Sharmeen <shatimbh@charlotte.edu>
2024-11-04 20:09:43 -0500

    Fixed footer links

--- bookstore-ui/src/components/Footer.js
+++ bookstore-ui/src/components/Footer.js
@@ -1,5 +1,6 @@
 // src/components/Footer.js
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import '../stylesheets/Footer.css';
 
 const Footer = () => {
@@ -14,28 +15,28 @@ const Footer = () => {
             <div className="logo-section">
                 <img src={`${process.env.PUBLIC_URL}/bookstorelogo.png`} alt="Logo" className="footer-logo" />
                 <div className="social-icons">
-                    <img src={`${process.env.PUBLIC_URL}/twitter.png`} alt="Social 1" />
-                    <img src={`${process.env.PUBLIC_URL}/insta.png`} alt="Social 2" />
-                    <img src={`${process.env.PUBLIC_URL}/yt.png`} alt="Social 3" />
+                    <img src={`${process.env.PUBLIC_URL}/twitter.png`} alt="Twitter" />
+                    <img src={`${process.env.PUBLIC_URL}/insta.png`} alt="Instagram" />
+                    <img src={`${process.env.PUBLIC_URL}/yt.png`} alt="YouTube" />
                 </div>
             </div>
             <div className="footer-section">
                 <h4>Explore</h4>
                 <ul>
-                    <li><a href="#genres">Genres</a></li>
-                    <li><a href="#recommended">Recommended</a></li>
-                    <li><a href="#non-book-items">Non-Book Items</a></li>
-                    <li><a href="#sales">Sales</a></li>
-                    <li><a href="#blog">Blog</a></li>
+                    <li><Link to="/genres">Genres</Link></li>
+                    <li><Link to="/recommendations">Recommended</Link></li>
+                    <li><Link to="/search">Non-Book Items</Link></li>
+                    <li><Link to="/sales">Sales</Link></li>
+                    <li><Link to="/blog">Blog</Link></li>
                 </ul>
             </div>
             <div className="footer-section">
                 <h4>Resources</h4>
                 <ul>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#store-policies">Store Policies</a></li>
-                    <li><a href="#hours">Hours</a></li>
-                    <li><a href="#contact-us">Contact Us</a></li>
+                    <li><Link to="/about">About</Link></li>
+                    <li><Link to="/store-policies">Store Policies</Link></li>
+                    <li><Link to="/hours">Hours</Link></li>
+                    <li><Link to="/contact-us">Contact Us</Link></li>
                     <li>
                         <select value={language} onChange={handleLanguageChange}>
                             <option value="English">English</option>
